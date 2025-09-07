e95035fb36758d8b46eab5c3640d07a0b3790110
Артем Бухряков <artembuhryakov332@gmail.com>
2025-09-07 14:57:48 +0500

    Task1

---
+++ Tyuiu.BukhryakovAY.Sprint0.Task1.Vo/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int age = 18;
+        String name = "Артем";
+        Console.WriteLine("Здравствуйте,меня зовут " + name + ",мне " +  age + " лет!");
+        Console.ReadKey();
+    }
+
+}
