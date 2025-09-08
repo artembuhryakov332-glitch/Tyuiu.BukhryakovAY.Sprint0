b7221867a277746c3b9f349a0de0eb5c0171b1c5
Артем Бухряков <artembuhryakov332@gmail.com>
2025-09-08 22:23:32 +0500

    Task5

---
+++ Tyuiu.BukhryakovAY.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿namespace Tyuiu.BukhryakovAY.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition (int a , int b)
+        {
+            return a + b;
+        }
+        public static int Subtruction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = 0 на ноль делить нельзя");
+                return -1;
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.BukhryakovAY.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.BukhryakovAY.Sprint0.Task5.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(10, 2));
+        Console.WriteLine(DataService.Subtruction(7, 2));
+        Console.WriteLine(DataService.Multiplication(15, 3));
+        Console.WriteLine(DataService.Division(10, 0));
+        Console.ReadKey();
+    }
+}
